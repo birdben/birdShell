96e3b49ad36c48519af22cd80699bafda6616dd0
liuben <191654006@163.com>
2016-08-20 15:26:20 +0800

    扩展logstash安装脚本，支持多个版本扩展

--- elk/install/logstash_shipper/logstash_shipper.sh
+++ elk/install/logstash_shipper/logstash_shipper.sh
@@ -1,32 +1,48 @@
 #!bin/bash
 
-software_path=$1
+# ----------------------------------------------------------------------
+# name:         logstash_shipper.sh
+# version:      1.1
+# createTime:   2016-08-11
+# description:  logstash_shipper环境安装
+# author:       birdben
+# email:        191654006@163.com
+# github:       https://github.com/birdben
+# ----------------------------------------------------------------------
+
+install_path=$1
 
 ################# Logstash install #################
-echo "安装目录是：$software_path"
-echo "################# Logstash install start #################"
+echo "安装目录是：$install_path"
+echo "################# Logstash_shipper install start #################"
+
+# logstash基础版本配置
+logstash_base_version='1.x'
+
+# 1.x版本配置
 logstash_version='1.5.6'
+
 logstash_tar='logstash-'${logstash_version}'.tar.gz'
-logstash_home_before=$software_path'/logstash-'${logstash_version}
-logstash_home=$software_path'/logstash-shipper-'${logstash_version}
-logstash_config=$software_path'/logstash-shipper.conf'
-logstash_config_name='logstash-shipper.conf'
+logstash_home_before=$install_path'/logstash-'${logstash_version}
+logstash_home=$install_path'/logstash-shipper-'${logstash_version}
+logstash_config=$install_path'/logstash-shipper.conf'
+logstash_config_file='logstash-shipper.conf'
 
 if [ -d $logstash_home ]; then
   echo "delete $logstash_home"
   rm -rf $logstash_home
 fi
 
-if [ ! -f $logstash_tar ]; then
+if [ ! -f $logstash_base_version/$logstash_tar ]; then
   echo "logstash tar not found - downloading $logstash_tar..."
   curl -o $logstash_tar https://download.elastic.co/logstash/logstash/$logstash_tar
 fi
 
-logstash_install_cmd="tar -zxf $logstash_tar -C $software_path/ && mv $logstash_home_before $logstash_home && mkdir -p $logstash_home/conf/ && cp $logstash_config_name $logstash_home/conf/ && cd $logstash_home && nohup ./bin/logstash -f conf/$logstash_config_name &"
+logstash_install_cmd="tar -zxf $logstash_base_version/$logstash_tar -C $install_path/ && mv $logstash_home_before $logstash_home && mkdir -p $logstash_home/conf/ && cp $logstash_base_version/$logstash_config_file $logstash_home/conf/ && cd $logstash_home && nohup ./bin/logstash -f conf/$logstash_config_file &"
 echo "running $logstash_install_cmd"
 # 执行安装logstash命令
 eval ${logstash_install_cmd}
-echo "################# Logstash install end #################"
+echo "################# Logstash_shipper install end #################"
 
 sleep 30
 echo "################# check logstash process start #################"
