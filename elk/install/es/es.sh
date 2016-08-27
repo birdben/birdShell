6af994a22cbfb7c3b38668b00f0e1d6e643b7425
birdben <191654006@163.com>
2016-08-27 19:35:23 +0800

    兼容ES安装2.3.5，2.2.1，2.0.0，1.7.3这几个版本安装，以及对ik和head插件的支持

--- elk/install/es/es.sh
+++ elk/install/es/es.sh
@@ -12,31 +12,71 @@
 
 install_path=$1
 
-################# ES install #################
-echo "安装目录是：$install_path"
-echo "################# ES install start #################"
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# es的版本和ik插件的版本
+es_ik_config_tar='ik.tar.gz'
 
-# es基础版本配置
-#es_base_version='1.x'
-es_base_version='2.x'
+# 
+es_version='1.7.3'
+es_ik_version='1.4.1'
 
-# 1.x版本配置
-#es_version='1.7.3'
-#es_head_plugin='elasticsearch-head-1.x.zip'
-#es_ik_plugin='elasticsearch-analysis-ik-1.4.1.jar'
-#es_ik_config_tar='ik.tar.gz'
-#es_httpclient_jar='httpclient-4.5.jar'
-#es_httpcore_jar='httpcore-4.4.1.jar'
+# 已测试
+#es_version='2.3.5'
+#es_ik_version='1.9.5'
 
-# 2.x版本配置
-es_version='2.3.5'
-es_head_plugin='elasticsearch-head-master.zip'
-es_ik_plugin='elasticsearch-analysis-ik-1.9.5.zip'
+# 已测试
+#es_version='2.2.1'
+#es_ik_version='1.8.1'
 
+# 已测试
+#es_version='2.0.0'
+#es_ik_version='1.5.0'
+
+if [[ $es_version =~ 1.* ]]; then
+  es_base_version='1.x'
+  es_head_version='1.x'
+fi
+if [[ $es_version =~ 2.* ]]; then
+  es_base_version='2.x'
+  es_head_version='master'
+fi
+# --------------------------------------------------------------------------------------------------------------------------------------------
 # 通用的参数
+es_skip_ik_plugin='no'
+es_skip_head_plugin='no'
 es_tar='elasticsearch-'${es_version}'.tar.gz'
 es_home=$install_path'/elasticsearch-'${es_version}
 es_config_file='elasticsearch.yml'
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# 安装head插件命令
+if [ $es_head_version = '1.x' ]; then
+  es_install_head_plugin_cmd='cp '$es_base_version'/elasticsearch-head-'${es_head_version}'.zip '$install_path'/ && cd '$es_home' && ./bin/plugin --install head --url file://'$install_path'/elasticsearch-head-'${es_head_version}'.zip'
+fi
+if [ $es_head_version == 'master' ]; then
+  es_install_head_plugin_cmd='cp '$es_base_version'/elasticsearch-head-'${es_head_version}'.zip '$install_path'/ && cd '$es_home' && ./bin/plugin install file://'$install_path'/elasticsearch-head-'${es_head_version}'.zip'
+fi
+
+# 安装ik插件命令
+if [[ $es_ik_version =~ 1.4.* ]]; then
+  es_install_ik_plugin_cmd='mkdir -p '$es_home'/plugins/ik && unzip -o '$es_base_version'/elasticsearch-analysis-ik-'${es_ik_version}'.zip -d '$es_home'/plugins/ik/ && tar -zxf '$es_base_version'/'$es_ik_config_tar' -C '$es_home'/config/'
+fi
+if [[ $es_ik_version =~ 1.5.* ]]; then
+  es_install_ik_plugin_cmd='mkdir -p '$es_home'/plugins/ik && unzip -o '$es_base_version'/elasticsearch-analysis-ik-'${es_ik_version}'.zip -d '$es_home'/plugins/ik/ && tar -zxf '$es_base_version'/'$es_ik_config_tar' -C '$es_home'/config/'
+fi
+if [[ $es_ik_version =~ 1.8.* ]]; then
+  es_install_ik_plugin_cmd='mkdir -p '$es_home'/plugins/ik && unzip -o '$es_base_version'/elasticsearch-analysis-ik-'${es_ik_version}'.zip -d '$es_home'/plugins/ik/'
+fi
+if [[ $es_ik_version =~ 1.9.* ]]; then
+  es_install_ik_plugin_cmd='mkdir -p '$es_home'/plugins/ik && unzip -o '$es_base_version'/elasticsearch-analysis-ik-'${es_ik_version}'.zip -d '$es_home'/plugins/ik/'
+fi
+
+# 安装ES命令
+es_install_cmd="tar -zxf $es_base_version/$es_tar -C $install_path/ && cp $es_base_version/$es_config_file $es_home/config/"
+es_startup_cmd="cd $es_home && ./bin/elasticsearch -d"
+# --------------------------------------------------------------------------------------------------------------------------------------------
+################# ES install #################
+echo "安装目录是：$install_path"
+echo "################# ES install start #################"
 
 if [ -d $es_home ]; then
   echo "delete $es_home"
@@ -44,42 +84,48 @@ if [ -d $es_home ]; then
 fi
 
 if [ ! -f $es_base_version/$es_tar ]; then
-  echo "elasticsearch tar not found - downloading $es_tar..."
-  curl -o $es_base_version/$es_tar https://download.elastic.co/elasticsearch/elasticsearch/$es_tar
-fi
-
-if [ ! -f $es_base_version/$es_head_plugin ]; then
-  echo "elasticsearch-head zip not found - downloading $es_head_plugin..."
+  echo "$es_tar not found - downloading $es_tar..."
   if [ $es_base_version = '1.x' ]; then
-  	curl -o $es_base_version/$es_head_plugin https://codeload.github.com/mobz/elasticsearch-head/zip/1.x
+    curl -o $es_base_version/$es_tar https://download.elastic.co/elasticsearch/elasticsearch/$es_tar
   fi
   if [ $es_base_version = '2.x' ]; then
-  	curl -o $es_base_version/$es_head_plugin https://codeload.github.com/mobz/elasticsearch-head/zip/master
+    curl -o $es_base_version/$es_tar https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${es_version}/$es_tar
   fi
 fi
 
-es_install_cmd="tar -zxf $es_base_version/$es_tar -C $install_path/"
-es_install_1_x_ik_plugin_cmd="tar -zxf $es_base_version/$es_ik_config_tar -C $es_home/config/ && cp $es_base_version/$es_ik_plugin $es_base_version/$es_httpclient_jar $es_base_version/$es_httpcore_jar $es_home/lib/ && cp $es_base_version/$es_config_file $es_home/config/"
-es_install_2_x_ik_plugin_cmd="mkdir -p $es_home/plugins/ik && unzip -o $es_base_version/$es_ik_plugin -d $es_home/plugins/ik/ && cp $es_base_version/$es_config_file $es_home/config/"
-es_install_1_x_head_plugin_cmd="cp $es_base_version/$es_head_plugin $install_path/ && cd $es_home && ./bin/plugin --install head --url file://$install_path/$es_head_plugin"
-es_install_2_x_head_plugin_cmd="cp $es_base_version/$es_head_plugin $install_path/ && cd $es_home && ./bin/plugin install file://$install_path/$es_head_plugin"
-es_startup_cmd="cd $es_home && ./bin/elasticsearch -d"
-echo "running $es_install_cmd"
+if [ ! -f $es_base_version/elasticsearch-head-${es_head_version}.zip ]; then
+  echo "$es_base_version/elasticsearch-head-${es_head_version}.zip not found - downloading elasticsearch-head-${es_head_version}.zip..."
+  if [ $es_head_version = '1.x' ]; then
+  	curl -o $es_base_version/'elasticsearch-head-'${es_head_version}'.zip' https://codeload.github.com/mobz/elasticsearch-head/zip/1.x
+  fi
+  if [ $es_head_version = 'master' ]; then
+  	curl -o $es_base_version/'elasticsearch-head-'${es_head_version}'.zip' https://codeload.github.com/mobz/elasticsearch-head/zip/master
+  fi
+fi
+
+if [ ! -f $es_base_version/elasticsearch-analysis-ik-${es_ik_version}.zip ]; then
+  echo "$es_base_version/elasticsearch-analysis-ik-${es_ik_version}.jar not found - skip install elasticsearch-analysis-ik-${es_ik_version}.jar..."
+  es_skip_ik_plugin='yes'
+fi
+# --------------------------------------------------------------------------------------------------------------------------------------------
 # 执行安装es命令
+echo "running $es_install_cmd"
 eval ${es_install_cmd}
-if [ $es_base_version = '1.x' ]; then
-  echo "安装1.x版本的插件"
-  eval ${es_install_1_x_ik_plugin_cmd}
-  eval ${es_install_1_x_head_plugin_cmd}
+if [ $es_skip_ik_plugin = 'no' ]; then
+  # 安装ik插件
+  echo "running ${es_install_ik_plugin_cmd}"
+  eval ${es_install_ik_plugin_cmd}
 fi
-if [ $es_base_version = '2.x' ]; then
-  echo "安装2.x版本的插件"
-  eval ${es_install_2_x_ik_plugin_cmd}
-  eval ${es_install_2_x_head_plugin_cmd}
+if [ $es_skip_head_plugin = 'no' ]; then
+  # 安装head插件
+  echo "running ${es_install_head_plugin_cmd}"
+  eval ${es_install_head_plugin_cmd}
 fi
+sleep 5
+echo "running ${es_startup_cmd}"
 eval ${es_startup_cmd}
 echo "################# ES install end #################"
-
+# --------------------------------------------------------------------------------------------------------------------------------------------
 sleep 30
 echo "################# check elasticsearch process start #################"
 ps -ef | grep elasticsearch
--- elk/install/jenkins_scp.sh
+++ elk/install/jenkins_scp.sh
@@ -11,18 +11,43 @@
 # ----------------------------------------------------------------------
 
 <<COMMENT
-# 安装软件目录/software/
-# 创建软连接目录/usr/local指向/software目录
-# 这样用jdk举例，安装目录始终是/software/jdk，但是java_home环境变量始终是/usr/local/java
-# 因为/usr/local/java -> /software/jdk软连接指向的是实际安装的目录
-
-# 修改elasticsearch.yml, logstash.conf, kibana.yml配置文件
-# java环境变量配置
-# sudo apt-get install unzip
-# sudo apt-get update
-# sudo apt-get install build-essential
-# /etc/bashrc 或者 /etc/bash.bashrc
-
+--------------------------------------------------------------------------------------------------------------------------------------------
+==== java环境变量配置 ====
+安装软件目录/software/，创建软连接目录/usr/local指向/software目录
+这样用jdk举例，安装目录始终是/software/jdk，但是java_home环境变量始终是/usr/local/java
+因为/usr/local/java -> /software/jdk软连接指向的是实际安装的目录
+--------------------------------------------------------------------------------------------------------------------------------------------
+==== 其他工具的安装（这里需要根据不同的系统自己修改） ====
+1.安装unzip解压工具
+  sudo apt-get install unzip
+2.安装make工具
+  sudo apt-get update
+  sudo apt-get install build-essential
+--------------------------------------------------------------------------------------------------------------------------------------------
+==== 修改各个组件的shell脚本中的版本信息 ====
+1.需要修改xxx_base_version主版本号
+2.需要修改xxx_version小版本号
+3.需要修改对应插件的版本信息（注意：这里可能每个ES版本对应的插件版本都不一样，需要看具体插件的github上对应版本来决定安装哪个版本）
+--------------------------------------------------------------------------------------------------------------------------------------------
+==== 配置文件的修改 ====
+1.修改elasticsearch.yml
+  network.host : 指定可以访问ES的IP地址（1.x版本不需要修改）
+  discovery.zen.ping.unicast.hosts : 单播的ES集群地址（只有集群搭建的时候需要）
+2.修改logstash-shipper.conf
+  input-file : log日志文件路径
+  output-redis : redis的IP地址，port端口号，pass密码
+3.修改logstash-indexer.conf
+  intput-redis : redis的IP地址，port端口号，pass密码
+  filter-grok : grok表达式
+  output-elasticsearch : es的IP地址
+4.kibana.yml配置文件
+  host : 可访问Kibana的IP地址
+  elasticsearch_url : 绑定ES的IP地址
+--------------------------------------------------------------------------------------------------------------------------------------------
+==== 修改我们command.log日志相关配置 ====
+1.需要将下面的代码配置到/etc/bashrc 或者 /etc/bash.bashrc，根据自己的系统环境配置
+2.注意配置下面代码片段中的日志输出路径，我这里配置的是：/software/command.log
+```
 # HISTDIR是Command命令保存的log文件路径，这里需要注意当前操作用户一定要对该log文件路径有读写权限
 HISTDIR='/software/command.log'
 if [ ! -f $HISTDIR ];
@@ -34,6 +59,8 @@ export HISTTIMEFORMAT="{\"TIME\":\"%F %T\",\"HOSTNAME\":\"$HOSTNAME\",\"LI\":\"$
 
 # 输出日志到指定的log文件
 export PROMPT_COMMAND='history 1|tail -1|sed "s/^[ ]\+[0-9]\+ //"|sed "s/$/\"}/">> /software/command.log'
+```
+--------------------------------------------------------------------------------------------------------------------------------------------
 COMMENT
 
 # 脚本和tar包在本地主机的存放路径
