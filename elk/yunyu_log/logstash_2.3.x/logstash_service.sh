6768d74f24653220a82120e8290689e88174c280
birdben <191654006@163.com>
2016-12-07 17:32:46 +0800

    规范Logstash配置文件命名规则，整理目录结构

--- elk/yunyu_log/logstash_2.3.x/logstash_service.sh
+++
@@ -1,2 +0,0 @@
-nohup logstash -f ${LOGSTASH_HOME}/conf/logstash-api.conf 2>${LOGSTASH_HOME}/bin/logstash.out 1>${LOGSTASH_HOME}/bin/logstash.out &
-#nohup logstash -f ${LOGSTASH_HOME}/conf/logstash-go.conf 2>${LOGSTASH_HOME}/bin/logstash.out 1>${LOGSTASH_HOME}/bin/logstash.out &
